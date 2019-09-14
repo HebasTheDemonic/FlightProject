057eee8882a712846860ce604c891168cb91f9e4
HebasTheDemonic <44752168+HebasTheDemonic@users.noreply.github.com>
2019-09-14 20:25:03 +0300

    Create HiddenFacade.cs

---
+++ FlightProject/Facades/HiddenFacade.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FlightProject.DAOs;
+
+namespace FlightProject.Facades
+{
+    public class HiddenFacade
+    {
+        GeneralDAOMSSQL generalDAO;
+
+        internal HiddenFacade()
+        {
+            generalDAO = new GeneralDAOMSSQL();
+        }
+
+        public void clearDb()
+        {
+            generalDAO.DBClear();
+        }
+
+        public void DbTestPrep()
+        {
+            generalDAO.DBTestPrep();
+        }
+
+        public void CleanFlightList()
+        {
+            generalDAO.CleanFlightList();
+        }
+    }
+
+   
+}
