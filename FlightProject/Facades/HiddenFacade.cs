3dc32feeb9a56efed9bab0c23fcd6479118fd5cd
HebasTheDemonic <44752168+HebasTheDemonic@users.noreply.github.com>
2020-02-08 16:45:14 +0200

    Update HiddenFacade.cs

--- FlightProject/Facades/HiddenFacade.cs
+++ FlightProject/Facades/HiddenFacade.cs
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -34,3 +35,39 @@ namespace FlightProject.Facades
 
    
 }
+=======
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FlightProject.DAOs;
+
+namespace FlightProject.Facades
+{
+    class HiddenFacade
+    {
+        GeneralDAOMSSQL generalDAO;
+
+        internal HiddenFacade()
+        {
+            generalDAO = new GeneralDAOMSSQL();
+        }
+
+        public void clearDb()
+        {
+            generalDAO.DBClear();
+        }
+
+        public void DbTestPrep()
+        {
+            generalDAO.DBTestPrep();
+        }
+
+        public void CleanFlightList()
+        {
+            generalDAO.CleanFlightList();
+        }
+    }
+}
+>>>>>>> 62807a85e985d18b4f08d83f3533f802794deebb
