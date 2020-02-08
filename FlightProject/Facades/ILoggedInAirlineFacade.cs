5b16b7b349f35c368ce86f8d75c976e6d9ff3a91
HebasTheDemonic <44752168+HebasTheDemonic@users.noreply.github.com>
2020-02-08 16:45:40 +0200

    Update ILoggedInAirlineFacade.cs

--- FlightProject/Facades/ILoggedInAirlineFacade.cs
+++ FlightProject/Facades/ILoggedInAirlineFacade.cs
@@ -14,7 +14,7 @@ namespace FlightProject.Facades
         IList<Flight> GetAllFlightsByAirline(LoginToken<AirlineCompany> token);
         void CancelFlight(LoginToken<AirlineCompany> token, int flightId);
         void CreateFlight(LoginToken<AirlineCompany> token, Flight flight);
-        void UpdateFlight(LoginToken<AirlineCompany> token, int flightId, Flight flight);
+        void UpdateFlight(LoginToken<AirlineCompany> token, int flightId, Flight flight); 
         void ChangeMyPassword(LoginToken<AirlineCompany> token, string OldPassword, string NewPassword);
         void ModifyAirlineDetails(LoginToken<AirlineCompany> token, AirlineCompany airline);
     }
